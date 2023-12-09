255d871d70c6866db2959a4f8b65c5e3682b60a7
Jay Reddy <jayreddy@gmail.com>
2023-12-08 18:13:14 -0800

    first try correct solution finishes day 1

--- day_1/p2/main.go
+++ day_1/p2/main.go
@@ -8,7 +8,18 @@ import (
 	"strconv"
 )
 
-// var stringToDigitMapping
+var stringToDigitMapping = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func main() {
 	f, err := os.Open("../puzzle_input.txt")
 	defer f.Close()
@@ -21,6 +32,9 @@ func main() {
 	for scanner.Scan() {
 		ln := scanner.Text()
 		frontNum := re.FindString(ln)
+		if len(frontNum) == 0 {
+			log.Printf("could not find digit in line: %v", ln)
+		}
 		var backNum string
 		var foundBackNum bool
 		for i := len(ln) - 1; foundBackNum == false; i-- {
@@ -30,11 +44,16 @@ func main() {
 			}
 		}
 		if len(frontNum) > 1 {
+			frontNum = stringToDigitMapping[frontNum]
+		}
+		if len(backNum) > 1 {
+			backNum = stringToDigitMapping[backNum]
 		}
 		num, err := strconv.Atoi(frontNum + backNum)
 		if err != nil {
-			log.Fatalf("convert nums in line from string to int: %v", err)
+			log.Fatalf("error converting nums in line from string to int: %v", err)
 		}
 		sum += num
 	}
+	log.Printf("final sum is %d", sum)
 }
