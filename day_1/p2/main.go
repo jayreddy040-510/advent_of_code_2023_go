f43495feed66484fa37545baf8661a6a8df20092
Jay Reddy <jayreddy@gmail.com>
2023-12-08 17:27:50 -0800

    almost finishes regexp port

--- day_1/p2/main.go
+++ day_1/p2/main.go
@@ -8,17 +8,33 @@ import (
 	"strconv"
 )
 
+// var stringToDigitMapping
 func main() {
 	f, err := os.Open("../puzzle_input.txt")
 	defer f.Close()
 	if err != nil {
 		log.Fatalf("error reading file: %v", err)
 	}
+	var sum int
+	re := regexp.MustCompile(`(one|two|three|four|five|six|seven|eight|nine|[1-9])`)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		ln := scanner.Text()
-		var foundFrontNum, foundBackNum bool
-		for i, j := 0, len(ln)-1; i <= j; {
+		frontNum := re.FindString(ln)
+		var backNum string
+		var foundBackNum bool
+		for i := len(ln) - 1; foundBackNum == false; i-- {
+			backNum = re.FindString(ln[i:])
+			if len(backNum) > 0 {
+				break
+			}
 		}
+		if len(frontNum) > 1 {
+		}
+		num, err := strconv.Atoi(frontNum + backNum)
+		if err != nil {
+			log.Fatalf("convert nums in line from string to int: %v", err)
+		}
+		sum += num
 	}
 }
