4fa0f246f2bfb2e775e7fa29d79b539619158b3b
Jay Reddy <jayreddy@gmail.com>
2023-12-15 08:10:52 -0800

    starts byte manipulation day_3

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -1,8 +1,29 @@
 package day3
 
 import (
-    "os"
-    "log"
-    "bufio"
-    "strconv"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
 )
+
+func PuzzleOne() {
+	f, err := os.Open("puzzle_input.txt")
+	if err != nil {
+		log.Fatalf("error opening puzzle input: %v", err)
+	}
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	var y int
+	for sc.Scan() {
+		b := sc.Bytes()
+		var byteSlice []byte
+		for i, x := range b {
+			if x >= '0' && x <= '9' {
+				// handle multi digit int check
+				byteSlice = append(byteSlice, x)
+			}
+		}
+
+	}
+}
